5dc28924f36f1f93ee1cf70f0e424cf95d52f5ea
Trevor Moore <trevorjaxon@yahoo.com>
2024-01-19 16:20:05 -0600

    Authenticator progressing

--- api/queries/accounts.py
+++ api/queries/accounts.py
@@ -1,24 +1,62 @@
 from pydantic import BaseModel
-from queries.pool import Queries
+from queries.pool import pool
+
+# from queries.pool import Queries
 
 
 class DuplicateAccountError(ValueError):
     pass
+
 
 class AccountIn(BaseModel):
     email: str
     password: str
     full_name: str
 
+
 class AccountOut(BaseModel):
-    id: str
+    id: int
     email: str
     full_name: str
 
 
-class AccountQueries(Queries):
-    def get(self, email:str) -> AccountOut:
+class AccountOutWithPassword(BaseModel):
+    id: int
+    email: str
+    full_name: str
+    hashed_password: str
+
+
+class AccountQueries:
+    def get(self, email: str) -> AccountOutWithPassword:
         pass
 
-    def create(self, info: AccountIn, hashed_password: str) -> AccountOut:
-        pass
+    def create(
+        self, info: AccountIn, hashed_password: str
+    ) -> AccountOutWithPassword:
+        with pool.connection() as conn:
+            with conn.cursor() as db:
+                result = db.execute(
+                    """
+                    INSERT INTO Accounts
+                        (
+                            email,
+                            password,
+                            full_name
+
+                        )
+                    VALUES
+                        (%s, %s, %s)
+                    RETURNING email, password, full_name;
+                    """,
+                    [info.email, hashed_password, info.full_name],
+                )
+                id = result.fetchone()[0]
+                print(
+                    id,
+                    "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$",
+                )
+                old_data = info.dict()
+                return AccountOutWithPassword(
+                    id=id, hashed_password=hashed_password, **old_data
+                )
