ad5480523be8fab9b41f30d7fadc37f33fa74e90
LaTroy <Latroyr26@gmail.com>
2024-01-19 08:55:49 -0500

    " Added a comma to fix the migration error "

--- api/migrations/001_Activities_tables.py
+++ api/migrations/001_Activities_tables.py
@@ -1,8 +1,10 @@
 steps = [
+    # step 1
     [
         # "Up" SQL statement
         """
         CREATE TABLE categories (
+            id SERIAL PRIMARY KEY NOT NULL,
             name VARCHAR(255) NOT NULL
         );
         """,
@@ -10,6 +12,9 @@ steps = [
         """
         DROP TABLE categories;
         """
+    ],
+    # step 2
+    [
         """
         CREATE TABLE Activities (
             name VARCHAR(255) NOT NULL,
@@ -17,7 +22,7 @@ steps = [
             start_date DATE NOT NULL,
             end_date DATE NOT NULL,
             location VARCHAR(255) NOT NULL,
-            category TEXT REFERENCES categories (name)
+            category INT REFERENCES categories(id)
         );
         """,
         # "Down" SQL statement
