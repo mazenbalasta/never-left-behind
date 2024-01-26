42c81ed157ccb04ddd1a867b09bad8876f5dfd5b
Trevor Moore <trevorjaxon@yahoo.com>
2024-01-26 11:42:43 -0600

    fixed front end error

--- ghi/src/HomePage.jsx
+++ ghi/src/HomePage.jsx
@@ -1,20 +1,19 @@
-function Resources() {
+function HomePage() {
     return (
         <div>
             <div className="App">
             <header className="App-header">
                 <div>
-                    <h1>Welcome to our Resources page!!!</h1>
+                    <h1>Welcome to our home page!!!</h1>
                 </div>
                 <h1 className="justify-center underline">
                     Never Left Behind
                 </h1>
-                <h1>This is where our links to your resources will be stored!</h1>
-                <h2>Thanks for your visit!!</h2>
+                <h1>Under construction</h1>
+                <h2>Coming on (or before) Feb 12, 2024</h2>
             </header>
         </div>
         </div>
     )
 }
-
-export default Resources
+export default HomePage
