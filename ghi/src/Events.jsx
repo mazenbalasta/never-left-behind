73d2130219f09dad0a1d70f8cdf028170ac2f5d2
Trevor Moore <trevorjaxon@yahoo.com>
2024-01-27 01:43:43 -0600

    Added Events table in full, with color scheming

--- ghi/src/Events.jsx
+++ ghi/src/Events.jsx
@@ -66,56 +66,73 @@ function ShowEvent() {
     }
 
     return (
-        <div>
-            <h1 className="fw-bold shadow w-25 p-3 mb-5 bg-white rounded">Events</h1>
-            <table className="hover:table-fixed mx-5">
-                <thead>
-                    <tr className="mx-5">
-                        <th className="fs-3 px-4" scope="col">Event Title</th>
-                        <th className="fs-3 px-4" scope="col">Start Date</th>
-                        <th className="fs-3 px-4" scope="col">End Date</th>
-                        <th className="fs-3 px-4" scope="col">Description</th>
-                        <th className="fs-3 px-4" scope="col">City</th>
-                        <th className="fs-3 px-4" scope="col">State</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {events.map(event => (
-                        <tr
-                            className={`table-auto ${selectedEvent === event ? "bg-blue-200" : ""}`}
-                            key={event.id}
-                            onClick={() => handleEventClick(event)}
-                        >
-                            <td className="px-4 py-2">{event.event_title}</td>
-                            <td className="px-4 py-2">{event.start_date}</td>
-                            <td className="px-4 py-2">{event.end_date}</td>
-                            <td className="px-4 py-2">{event.description}</td>
-                            <td className="px-4 py-2">{event.city}</td>
-                            <td className="px-4 py-2">{}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+        <div className="flex flex-col bg-gray-900 w-screen">
+            <div className="shadow-md sm:rounded-lg"></div>
+            <div className="flex flex-col">
+                <div className="shadow-md sm:rounded-lg">
+                    <div className="inline-block min-w-full">
+                        <table className="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
+                            <thead className="bg-gray-900 dark:bg-gray-700">
+                                <tr>
+                                    <th scope="col" className="p-4"></th>
+                                    <th scope="col" className="py-3 px-6 text-s font-medium tracking-wider text-left text-gray-400 uppercase dark:text-gray-400">
+                                        Event Title
+                                    </th>
+                                    <th scope="col" className="py-3 px-6 text-s font-medium tracking-wider text-left text-gray-400 uppercase dark:text-gray-400">
+                                        Start Date
+                                    </th>
+                                    <th scope="col" className="py-3 px-6 text-s font-medium tracking-wider text-left text-gray-400 uppercase dark:text-gray-400">
+                                        End Date
+                                    </th>
+                                    <th scope="col" className="py-3 px-6 text-s font-medium tracking-wider text-left text-gray-400 uppercase dark:text-gray-400">
+                                        Description
+                                    </th>
+                                    <th scope="col" className="py-3 px-6 text-s font-medium tracking-wider text-left text-gray-400 uppercase dark:text-gray-400">
+                                        City
+                                    </th>
+                                    <th scope="col" className="py-3 px-6 text-s font-medium tracking-wider text-left text-gray-400 uppercase dark:text-gray-400">
+                                        State
+                                    </th>
+                                </tr>
+                            </thead>
+                            {events.map(event => (
+                                <tbody className="bg-gray-700 divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
+                                    <tr className="hover:bg-gray-600 dark:hover:bg-gray-700" onClick={() => handleEventClick(event)}>
+                                        <td className="p-4 w-4">
+                                            <div className="flex items-center"></div>
+                                        </td>
+                                        <td className="py-4 px-6 text-sm font-medium text-white whitespace-nowrap dark:text-white">{event.event_title}</td>
+                                        <td className="py-4 px-6 text-sm font-medium text-white whitespace-nowrap dark:text-white">{event.start_date}</td>
+                                        <td className="py-4 px-6 text-sm font-medium text-white whitespace-nowrap dark:text-white">{event.end_date}</td>
+                                        <td className="py-4 px-6 text-sm font-medium text-white whitespace-nowrap dark:text-white">{event.description}</td>
+                                        <td className="py-4 px-6 text-sm font-medium text-white whitespace-nowrap dark:text-white">{event.city}</td>
+                                        <td className="py-4 px-6 text-sm font-medium text-white whitespace-nowrap dark:text-white">{event.state.state_name}</td>
+                                    </tr>
+                                </tbody>
+                            ))}
+                        </table>
+                        {selectedEvent && (
+                            <div>
+                                <h2 className="text-white">Selected Event Details:</h2>
+                                <p className="text-white">Event Title: {selectedEvent.event_title}</p>
+                                <p className="text-white">Start Date: {selectedEvent.start_date}</p>
+                                <p className="text-white">End Date: {selectedEvent.end_date}</p>
+                                <p className="text-white">Description: {selectedEvent.description}</p>
+                                <p className="text-white">Street Address: {selectedEvent.street_address}</p>
+                                <p className="text-white">City: {selectedEvent.city}</p>
+                                <p className="text-white">State: {selectedEvent.state.state_name}</p>
 
-            {selectedEvent && (
-                <div>
-                    <h2>Selected Event Details:</h2>
-                    <p>Event Title: {selectedEvent.event_title}</p>
-                    <p>Start Date: {selectedEvent.start_date}</p>
-                    <p>End Date: {selectedEvent.end_date}</p>
-                    <p>Description: {selectedEvent.description}</p>
-                    <p>Street Address: {selectedEvent.street_address}</p>
-                    <p>City: {selectedEvent.city}</p>
-                    <p>State: {}</p>
-
-                    <div/>
-                    <div className="d-flex justify-content-start gap-2 ">
-                        <button className="btn btn-danger " onClick={handleDeleteClick}>Delete Event</button>
-                        <button className="btn btn-warning" onClick={handleUpdateClick}>Update a event</button>
-                        <button className="btn btn-primary" onClick={handleResetClick}>Reset</button>
+                                <div/>
+                                <div className="d-flex justify-content-start gap-2 ">
+                                    <button className="btn btn-danger text-white" onClick={handleDeleteClick}>Delete Event</button>
+                                    <button className="btn btn-warning text-white" onClick={handleUpdateClick}>Update an Event</button>
+                                    <button className="btn btn-primary text-white" onClick={handleResetClick}>Reset</button>
+                                </div>
+                            </div>
+                        )}
                     </div>
                 </div>
-            )}
+            </div>
         </div>
     );
 }
