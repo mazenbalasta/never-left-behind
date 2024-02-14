12e47a87ee20455a6244e4180fcc4496d27a5223
Trevor Moore <trevorjaxon@yahoo.com>
2024-02-14 14:45:27 -0600

    Fixed link tag, changed corshost on main to public url

--- api/main.py
+++ api/main.py
@@ -30,7 +30,7 @@ app.include_router(states.router, tags=["States"])
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.environ.get("CORS_HOST")],
+    allow_origins=[os.environ.get("PUBLIC_URL, http://localhost:5173")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
